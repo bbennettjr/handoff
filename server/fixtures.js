0bd4aeebc9205ae48460fdf8f734318e0a45ba4b
user <user@users-MacBook-Pro.local>
2017-07-11 18:40:32 -0400

    Dan's first push

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,3 +1,5 @@
+import Patients from "/imports/api/patients/patients.js"
+
 if (Patients.find().count() === 0) {
 	Patients.insert({
 		first: "Robert",
@@ -6,24 +8,24 @@ if (Patients.find().count() === 0) {
 		condition: "stable",
 		loc: "1",
 		diagnosis: "HIV Dementia",
-		createdAt: new Date
-	});
+		createdAt: new Date()
+	})
 	Patients.insert({
 		first: "Michael",
 		last: "Edgar",
 		dob: "5/20/1976",
 		condition: "tenuous",
 		loc: "2C",
-		diagnosis: "Acute Renal Failure"
-		createdAt: new Date
-	});
+		diagnosis: "Acute Renal Failure",
+		createdAt: new Date()
+	})
 	Patients.insert({
 		first: "Ella",
 		last: "Goldson",
 		dob: "11/07/1921",
 		condition: "stable",
 		loc: "2C",
-		diagnosis: "Failure to thrive"
-		createdAt: new Date
-	});
+		diagnosis: "Failure to thrive",
+		createdAt: new Date()
+	})
 }
