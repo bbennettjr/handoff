c90427e05c7de26ce4040875cad71dd6809d82e9
Bradford Bennett <bbennettjr@gmail.com>
2017-07-16 11:11:47 -0400

    Remove autopub and insecure

--- imports/ui/components/app/app.js
+++ imports/ui/components/app/app.js
@@ -1,22 +1,22 @@
 // Atmosphere and NPM
-import { Meteor } from "meteor/meteor"
-import React from "react"
-import PropTypes from "prop-types"
-import { compose } from "react-komposer"
-import { Grid } from "react-bootstrap"
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { compose } from 'react-komposer';
+import { Grid } from 'react-bootstrap';
 import {
 	BrowserRouter as Router,
 	Route,
 	Link,
 	Switch,
 	Redirect
-} from "react-router-dom"
+} from 'react-router-dom';
 
 // Components
-import { Navigation } from "../navigation/navigation.jsx"
-import PatientList from "../patients/patient_list/patient-list.jsx"
-import Patient from "../patients/patient/patient.jsx"
-import { NewPatient } from "../patients/patient/new-patient.js"
+import { Navigation } from '../navigation/navigation.jsx';
+import PatientList from '../patients/patient_list/patient-list.jsx';
+import Patient from '../patients/patient/patient.jsx';
+import { NewPatient } from '../patients/patient/new-patient.js';
 
 // App Component
 export const App = appProps => {
@@ -27,8 +27,6 @@ export const App = appProps => {
 				<Grid>
 					<Switch>
 						<Route path="/patient/:_id" component={Patient} />
-						<Route path="/team" component={Team} />
-						<Route path="/cover" component={Cover} />
 
 						<Route path="/newpatient" component={NewPatient} />
 						<Route path="/" component={PatientList} />
@@ -36,13 +34,5 @@ export const App = appProps => {
 				</Grid>
 			</div>
 		</Router>
-	)
-}
-
-// Placeholders
-const Team = props => {
-	return <h1>Your Team's Patients</h1>
-}
-const Cover = props => {
-	return <h1>Cover List</h1>
-}
+	);
+};
