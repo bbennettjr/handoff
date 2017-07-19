d4846f1513598253e63d53b1ce680c69b5db4b79
Brad Bennett <rennox4l@users.noreply.github.com>
2017-07-19 13:06:06 -0400

    Create rightButtons for AppBar

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -2,14 +2,20 @@ import React from "react"
 import { Link } from "react-router-dom"
 import AppBar from "material-ui/AppBar"
 import FlatButton from 'material-ui/FlatButton';
-import { Tabs, Tab } from "material-ui/Tabs"
 import AccountsWrapper from "../accounts/accounts-wrapper.js"
 
 export const Navigation = appProps => {
   return (
     <AppBar
       title="Handoff"
-      iconElementRight={<FlatButton label="New Patient" containerElement={<Link to="/newpatient"/>}/>}
+      iconElementRight={rightButtons}
     />
   )
 }
+
+const rightButtons = (
+  <div>
+  <FlatButton label="New Patient" containerElement={<Link to="/newpatient"/>}/>
+  <FlatButton label="New Patient" containerElement={<AccountsWrapper />}/>
+  </div>
+  )
