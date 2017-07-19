73e33ac2f6350ac38050cde04b1784d33886ffa4
Brad Bennett <rennox4l@users.noreply.github.com>
2017-07-19 12:57:28 -0400

    Try Link as containerElement of FlatButton

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { NavLink } from "react-router-dom"
+import { Link } from "react-router-dom"
 import AppBar from "material-ui/AppBar"
+import FlatButton from 'material-ui/FlatButton';
 import { Tabs, Tab } from "material-ui/Tabs"
 import AccountsWrapper from "../accounts/accounts-wrapper.js"
 
@@ -8,12 +9,7 @@ export const Navigation = appProps => {
   return (
     <AppBar
       title="Handoff"
-      children={
-        <Tabs>
-          <Tab label="New Patient" data-route="/newpatient" />
-          <Tab label="Home" data-route="/" />
-        </Tabs>
-      }
+      iconElementRight={<FlatButton label="New Patient" containerElement={<Link to="/newpatient"/>}/>}
     />
   )
 }
