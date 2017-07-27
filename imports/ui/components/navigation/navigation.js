077a082af55050bf9e9b453b6d25aa698c1898e2
Bradford Bennett <bbennettjr@gmail.com>
2017-07-27 08:42:43 -0400

    Toolbar to replace AppBar

--- imports/ui/components/navigation/navigation.js
+++ imports/ui/components/navigation/navigation.js
@@ -1,5 +1,11 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import {
+  Toolbar,
+  ToolbarGroup,
+  ToolbarSeparator,
+  ToolbarTitle
+} from "material-ui/Toolbar"
 import AppBar from "material-ui/AppBar"
 import FlatButton from "material-ui/FlatButton"
 import AccountsWrapper from "../accounts/accounts-wrapper.js"
@@ -16,24 +22,29 @@ const styles = {
 }
 
 export const Navigation = appProps => {
-  return <AppBar title={titleButtons} iconElementRight={rightButtons} />
+  return (
+    <Toolbar>
+      <Link to="/">
+        <ToolbarTitle text="Handoff" style={styles.title} />
+      </Link>
+      <ToolbarGroup lastChild={true}>
+        <FlatButton
+          label="New Patient"
+          labelStyle={styles.buttons}
+          containerElement={<Link to="/newpatient" />}
+        />
+        <FlatButton
+          label="Sign In"
+          labelStyle={styles.buttons}
+          containerElement={<AccountsWrapper />}
+        />
+      </ToolbarGroup>
+    </Toolbar>
+  )
 }
 
 const titleButtons = (
-  <Link to="/" style={styles.title}><span>Handoff</span></Link>
-)
-
-const rightButtons = (
-  <div>
-    <FlatButton
-      label="New Patient"
-      labelStyle={styles.buttons}
-      containerElement={<Link to="/newpatient" />}
-    />
-    <FlatButton
-      label="Sign In"
-      labelStyle={styles.buttons}
-      containerElement={<AccountsWrapper />}
-    />
-  </div>
+  <Link to="/" style={styles.title}>
+    <span>Handoff</span>
+  </Link>
 )
