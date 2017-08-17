ba1184515366ded60b768cac25bde12b07fce908
Bradford Bennett Jr <bbennettjr@gmail.com>
2017-08-17 16:57:12 -0400

    Start navigation controls

---
+++ imports/ui/components/navigation/navigation-controls/handoff.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ShareIcon from 'material-ui-icons/Share';
+import IconButton from 'material-ui/IconButton';
+
+const ShareButton = (props) => {
+  return(
+    <IconButton dense color='accent'
+    )
+}
