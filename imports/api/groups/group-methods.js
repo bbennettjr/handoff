a1081da3299cda6b704f877cbe46e9fd7db23cd0
Bradford Bennett Jr <bbennettjr@gmail.com>
2017-11-05 15:37:50 -0500

    wiring up methods to UI

--- imports/api/groups/group-methods.js
+++ imports/api/groups/group-methods.js
@@ -24,47 +24,51 @@ export const insertGroup = new ValidatedMethod({
         "You must sign in to create a group"
       )
     }
-    let insertedId = Group.insertOne(group)
+    let insertedId = Groups.insertOne(group)
     return { _id: insertedId }
   }
 })
 
 export const addToGroup = new ValidatedMethod({
   name: "addToGroup",
-  validate({ _groupId }) {
-    const result = v.validate(_groupId, schema)
+  validate({ _id }) {
+    const result = v.validate({ _id: _id }, schema)
     if (!result.valid) throw new ValidationError()
   },
-  run({ _groupId }) {
+  run({ _id }) {
     if (!this.userId) {
       throw new Meteor.Error(
         "Not authorized",
         "You must sign in to join a group"
       )
     }
-    let writeResult = Group.update(_groupId, {
-      $addToSet: { members: this.userId }
-    })
-    return writeResult.hasWriteError() ? writeResult.writeError : "success"
+    let result = Groups.update(
+      { _id: _id },
+      {
+        $addToSet: { members: this.userId }
+      }
+    )
+    return { result }
   }
 })
 
 export const removeFromGroup = new ValidatedMethod({
   name: "removeFromGroup",
-  validate({ _groupId }) {
-    const result = v.validate(_groupId, schema)
+  validate({ _id }) {
+    const result = v.validate(_id, schema)
     if (!result.valid) throw new ValidationError()
   },
-  run({ _groupId }) {
+  run({ _id }) {
     if (!this.userId) {
       throw new Meteor.Error(
         "Not authorized",
         "You must sign in to remove yourself from a group"
       )
     }
-    let writeResult = Group.update(_groupId, {
+    console.log(_id)
+    let result = Groups.update(_id, {
       $pull: { members: this.userId }
     })
-    return writeResult.hasWriteError() ? writeResult.writeError : "success"
+    return { result }
   }
 })
--- imports/ui/components/groups/GroupPage.js
+++ imports/ui/components/groups/GroupPage.js
@@ -2,20 +2,13 @@ import { Meteor } from "meteor/meteor"
 import React from "react"
 import PropTypes from "prop-types"
 import { Groups } from "/imports/api/groups/groups.js"
-import { insertGroup, addToGroup } from "/imports/api/groups/group-methods.js"
+import {
+  insertGroup,
+  addToGroup,
+  removeFromGroup
+} from "/imports/api/groups/group-methods.js"
 import { withTracker } from "meteor/react-meteor-data"
 import { Row, Col, Menu, Dropdown, Button, Icon, message } from "antd"
-
-function handleMenuClick({ key }) {
-  message.info(`${key === "1" ? "Joined" : "Left"} the team`)
-}
-
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">Join</Menu.Item>
-    <Menu.Item key="2">Leave</Menu.Item>
-  </Menu>
-)
 
 class GroupPage extends React.Component {
   static propTypes = {
@@ -26,16 +19,23 @@ class GroupPage extends React.Component {
     const COL_LENGTH = 4
     const groups = this.props.groups.slice()
     let start, end
+
+    const menu = key => (
+      <Menu onClick={this.handleMenuClick}>
+        <Menu.Item key={key}>Leave</Menu.Item>
+      </Menu>
+    )
+
     return (function rows(acc, arr) {
       return acc.map((r, i) => {
         start = i * COL_LENGTH
         end = start + COL_LENGTH
         return (
           <Row type="flex" justify="space-around" align="middle" key={i}>
-            {arr.slice(start, end).map(g => {
+            {arr.slice(start, end).map((g, i) => {
               return (
-                <Col key={g._id}>
-                  <Dropdown overlay={menu}>
+                <Col key={i}>
+                  <Dropdown overlay={menu(g._id)}>
                     <Button>
                       {g.name} <Icon type="down" />
                     </Button>
@@ -47,6 +47,19 @@ class GroupPage extends React.Component {
         )
       })
     })(new Array(Math.ceil(groups.length / COL_LENGTH)).fill(1), groups)
+  }
+
+  handleMenuClick({ key }) {
+    // const _id = key
+    // // key === "1"
+    // //   ? addToGroup.call({ key }, (err, res) => {
+    // //       if (err) console.error(err)
+    // //       message.info("Joined the team")
+    // //     })
+    // removeFromGroup.call({ _id }, (err, res) => {
+    //   if (err) console.error(err)
+    //   message.info("Left the team")
+    // })
   }
 
   render() {
